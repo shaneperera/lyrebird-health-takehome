9107081e28dd4358267cc011b545ad2fb3081765
Shane Perera <sptrad01@gmail.com>
2026-06-22 19:47:54 +1000

    feat: add Zod validation middleware for body and query

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { ZodSchema } from 'zod';
+
+export const validateBody = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.body = schema.parse(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export const validateQuery = (schema: ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.query = schema.parse(req.query);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
